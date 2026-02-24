576a913787a704278e45de2df55e2d44807cb269
come-wale-senusi <akinyemisaheedwale@gmail.com>
2026-02-24 22:55:19 +0100

    verify

--- app/(public)/verifynaira/page.tsx
+++ app/(public)/verifynaira/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-
-import Navbar from '@/components/navbar'
+import { Suspense } from "react"
 import VerifyPayment from "./verify";
 import Footer from "@/components/footer";
 
 export default function VerifyPaymentPage() {
   return (<>
     <main className="min-h-screen bg-gradient-to-br from-purple-700 via-indigo-700 to-blue-700 text-white">
-      <VerifyPayment />
+        <Suspense fallback={<div className="text-center py-20">Loading...</div>}>
+        <VerifyPayment />
+      </Suspense>
       <Footer />
     </main>
   </>)
